2135150b55e6f45f04597dd34a6472178b0024f6
Timothy <tsit20@stuy.edu>
2021-10-20 11:21:25 -0400

    continue

--- song_node.c
+++ song_node.c
@@ -23,21 +23,31 @@ struct song_node * insert_front(struct song_node *ne, char *a, char *s){
 
 struct song_node * insert_alpha(struct song_node *ne, char *a, char *s){
   struct song_node *n = make_node(a, s);
+  struct song_node *front = ne;
 	ne = compare_node(ne, n);
-  n->next = ne;
+	if (ne==0){
+		n->next = front;//node is in front
+	}
+	else{//node is not in front
+		if (ne->next){//node in middle
+			n->next = ne->next;
+		}
+		else{//node is last
+			n->next = 0;
+		}
+		ne->next = n;
+	}
   return n;
 }
 
 struct song_node * compare_node(struct song_node *list, struct song_node *node){
-	while (strcmp(tolower(list->artist), tolower(node->artist)) < 0){//list is earlier than node
-		if (strcmp(tolower(list->artist), tolower(node->artist)) > 0){//node is between the two in list
-
-		}
-		list = list->next;
+	if (strcmp((list->artist), (node->artist)) > 0){
+		return 0;//node's artist is before
 	}
-	if (strcmp(tolower(list->artist), tolower(node->artist)) > 0){//list is later than node
-
+	else while (strcmp((list->next->artist), (node->artist)) < 0){
+		list = list->next;//node's artist is after
 	}
+	return list;
 }
 
 void print_list(struct song_node *f){
